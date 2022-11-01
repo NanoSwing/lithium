43caff637d101903add203fafde574aa11c26f2d
Linus Kåreblom <linus.kareblom@gmail.com>
2022-11-01 13:08:30 +0100

    Added documentation to windowing.

--- lithium/include/platform/platform_windowing.h
+++ lithium/include/platform/platform_windowing.h
@@ -5,12 +5,18 @@
 #include "base/base_arena.h"
 #include <vulkan/vulkan.h>
 
+// Window handle.
 typedef struct LiWindow LiWindow;
 
+// Create a window.
 LIAPI LiWindow *liWindowCreate(LiArena *arena, U32 width, U32 height, const char *title, B8 resizable);
+// Destroy winodw.
 LIAPI void liWindowDestroy(LiWindow *window);
+// Poll for window events.
 LIAPI void liWindowPollEvents(LiWindow *window);
+// If a close event has been sent, this will return true.
 LIAPI B8 liWindowClosed(const LiWindow *window);
+// Get a vulkan surface from window.
 LIAPI VkSurfaceKHR liWindowGetSurface(const LiWindow *window, VkInstance instance);
 
 #endif // LI_PLATFORM_WINDOWING_H
