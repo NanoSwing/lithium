45ffe406d09b8ec4af1896dd5a678d5d9dc1cc63
Linus Kåreblom <linus.kareblom@gmail.com>
2022-11-01 13:07:08 +0100

    Added documentation to threading.

--- lithium/include/platform/platform_threading.h
+++ lithium/include/platform/platform_threading.h
@@ -5,18 +5,29 @@
 #include "base/base_defines.h"
 #include "base/base_arena.h"
 
+// Thread handle.
 typedef U64 LiThread;
+// Mutex handle.
 typedef struct LiMutex LiMutex;
+// Function ran by a thread.
 typedef void *(*LiThreadFunc)(void *arg);
 
+// Starts a new thread and returns the handle. Arg will be passed to thread_func.
 LIAPI LiThread liThreadCreate(LiThreadFunc thread_func, void *arg);
+// Destroys thread, handing it back to the OS.
 LIAPI void liThreadDestroy(LiThread thread);
+// Waits until thread has finished its operation. Thread_func return value will be placed in output if output isn't NULL.
 LIAPI void liThreadWait(LiThread thread, void **output);
+// Gets thread handle of current thread.
 LIAPI LiThread liThreadGetSelf(void);
 
+// Create mutex.
 LIAPI LiMutex *liMutexCreate(LiArena *arena);
+// Destroy mutex.
 LIAPI void liMutexDestroy(LiMutex *mutex);
+// Lock mutex.
 LIAPI void liMutexLock(LiMutex *mutex);
+// Unlock mutex.
 LIAPI void liMutexUnlock(LiMutex *mutex);
 
 #endif // LI_PLATFORM_THREADING_H
